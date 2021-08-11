4f7066b80819f1a95e2ec36ede119c4e232d30f1
jordy muniz <jordy.muniz96@gmail.com>
2021-08-11 09:54:51 -0500

    initial commit

---
+++ src/main/java/com/codeup/oberonblogapp/OberonblogappApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.oberonblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OberonblogappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OberonblogappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/oberonblogapp/OberonblogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.oberonblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OberonblogappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
